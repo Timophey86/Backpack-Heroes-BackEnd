ceb04e818169ccb93a883a5c3313f4245ae39ad5
Timophey86 <yotima121212@gmail.com>
2021-03-27 15:47:25 +0300

    atlas

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://tima:1234@cluster0.rbszt.mongodb.net/Backpack-HeroesDB?retryWrites=true&w=majority',
+  // 'dbURL': 'mongodb://localhost:27017',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://tima:1234@cluster0.rbszt.mongodb.net/Backpack-HeroesDB?retryWrites=true&w=majority',
 }
